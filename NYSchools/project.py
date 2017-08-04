acd820997925755ba217f14a47ccb13ac2a806ec
Evis Bregu <evis.bregu@gmail.com>
2017-08-04 11:03:14 +0200

    Finished data cleaning and merging

--- NYSchools/project.py
+++ NYSchools/project.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import numpy
 
 pd.set_option('display.width', 200)
 data_files = [
@@ -17,7 +18,6 @@ for file in data_files:
     data[data_key] = pd.read_csv("schools/" + file)
 
 # explore the sat_scores data set. print the first 5 rows
-
 for key in data:
     print(key)
     print(data[key].head())
@@ -88,3 +88,74 @@ data["hs_directory"]["lon"] = data["hs_directory"]["Location 1"].apply(lambda x:
 # Convert latitude and longitude values to numeric
 data["hs_directory"]["lat"] = pd.to_numeric(data["hs_directory"]["lat"], errors='coerce')
 data["hs_directory"]["lon"] = pd.to_numeric(data["hs_directory"]["lon"], errors='coerce')
+
+# Explore the class_size dataset
+print(data["class_size"])
+print(data["class_size"].keys())
+print(data["class_size"]["GRADE "].unique())
+print(data["class_size"]["PROGRAM TYPE"].unique())
+
+# Lets filter the class size dataset to only contain rows with GRADE '09-12' and PROGRAM TYPE 'GEN ED'
+class_size = data["class_size"]
+class_size = class_size[class_size["GRADE "] == '09-12']
+class_size = class_size[class_size["PROGRAM TYPE"] == 'GEN ED']
+print(class_size.head())
+
+# Group the dataframe by DBN, compute aggregated values using numpy.mean, reindex and reassign back to data dictionary
+class_size = class_size.groupby("DBN").agg({
+    'NUMBER OF STUDENTS / SEATS FILLED': numpy.mean,
+    'NUMBER OF SECTIONS': numpy.mean,
+    'AVERAGE CLASS SIZE': numpy.mean,
+    'SIZE OF SMALLEST CLASS': numpy.min,
+    'SIZE OF LARGEST CLASS': numpy.max,
+    'SCHOOLWIDE PUPIL-TEACHER RATIO': numpy.mean
+})
+
+class_size.reset_index(inplace=True)
+data['class_size'] = class_size
+print(data['class_size'].head())
+
+# Filter demographics, keep only data for schoolyear 20112012
+data["demographics"] = data["demographics"][data["demographics"]["schoolyear"] == 20112012]
+print(data["demographics"].head())
+
+# Filter graduation dataset and keep only records with Demographics 'Total Cohort' and latest Cohort of 2006
+data["graduation"] = data["graduation"][(data["graduation"]["Cohort"] == '2006') & (data["graduation"]["Demographic"] == 'Total Cohort')]
+print(data["graduation"].head())
+
+# Convert columns for the ap_2010 dataset to numeric values
+cols = ['AP Test Takers ', 'Total Exams Taken', 'Number of Exams with scores 3 4 or 5']
+
+for col in cols:
+    data["ap_2010"][col] = pd.to_numeric(data["ap_2010"][col], errors="coerce")
+print(data["ap_2010"].head())
+
+
+# Merge sat_results dataset with ap_2010 and graduation datasets. We will use a left join to preserve sat_results rows
+combined = data["sat_results"]
+combined = combined.merge(data["ap_2010"], how='left')
+combined = combined.merge(data["graduation"], how='left')
+
+print(combined.head())
+
+# Merge the combined dataset with class_size, demographics, survey, and hs_directory. This time we will use
+# inner join because this data sets have few missing DBNs
+combined = combined.merge(data["class_size"], on="DBN", how="inner")
+combined = combined.merge(data["demographics"], on="DBN", how="inner")
+combined = combined.merge(data["survey"], on="DBN", how="inner")
+combined = combined.merge(data["hs_directory"], on="DBN", how="inner")
+print(combined.shape)
+
+# lets fill nan values in the dataset with numbers. Try to fill the missing values with average values from that column.
+# First calculate mean values for dataset
+means = combined.mean()
+
+# Fill nan-s with mean values
+combined = combined.fillna(means)
+
+# Fill remaining nan-s with 0
+combined = combined.fillna(0)
+
+# Generate school district values (first 2 characters of DBN) and store them to the column "school_dist"
+# This will be usefull for mapping
+combined["school_dist"] = combined["DBN"].apply(lambda x: x[0:2])
